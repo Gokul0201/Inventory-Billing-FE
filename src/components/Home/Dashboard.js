386be607c692b441d0f8ea08deacf8d58852c854
GOKULAKRISHNAN K <kgokula314@gmail.com>
2022-11-08 09:07:21 +0530

    commit

--- src/components/Home/Dashboard.js
+++ src/components/Home/Dashboard.js
@@ -24,7 +24,6 @@ const Dashboard = () => {
 {/* <!-- Content Row --> */}
 <div className="row">
 
-    {/* <!-- Earnings (Monthly) Card Example --> */}
     <div className="col-xl-3 col-md-6 mb-4">
         <div className="card border-left-primary shadow h-100 py-2">
             <div className="card-body">
@@ -32,17 +31,16 @@ const Dashboard = () => {
                     <div className="col mr-2">
                         <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
                             Customers</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">Customers</div>
+                        <div className="h5 mb-0 font-weight-bold text-gray-800">2</div>
                     </div>
                     <div className="col-auto">
-                        <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                        <i className="fas  fa-people-line fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    {/* <!-- Earnings (Monthly) Card Example --> */}
     <div className="col-xl-3 col-md-6 mb-4">
         <div className="card border-left-success shadow h-100 py-2">
             <div className="card-body">
@@ -50,10 +48,10 @@ const Dashboard = () => {
                     <div className="col mr-2">
                         <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
                             Stocks</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">Stocks</div>
+                        <div className="h5 mb-0 font-weight-bold text-gray-800">2%</div>
                     </div>
                     <div className="col-auto">
-                        <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        <i className="fas fa-cubes-stacked fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
@@ -62,7 +60,7 @@ const Dashboard = () => {
 
     
 
-    {/* <!-- Pending Requests Card Example --> */}
+  
     <div className="col-xl-3 col-md-6 mb-4">
         <div className="card border-left-warning shadow h-100 py-2">
             <div className="card-body">
@@ -70,10 +68,10 @@ const Dashboard = () => {
                     <div className="col mr-2">
                         <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
                              Payments</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">Pending Payments</div>
+                        <div className="h5 mb-0 font-weight-bold text-gray-800">NIL</div>
                     </div>
                     <div className="col-auto">
-                        <i className="fas fa-comments fa-2x text-gray-300"></i>
+                        <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
